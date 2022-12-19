1b62ade6d7edfbc715330c54a03e1286a37b8c1a
cpplovelove <57889360+cpplovelove@users.noreply.github.com>
2022-12-20 00:12:09 +0900

    [SERVER][REFACTOR]typescript 적용 - typescript 적용

--- server/controller/index.ts
+++ server/controller/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import UserController from '../controller/user-controller.js';
+import UserController from './user-controller.js';
 
 const Router = express.Router();
 
