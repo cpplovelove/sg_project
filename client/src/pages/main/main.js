e0543e6dcf20496a1c2dc05df97e50edec0b7579
cpplovelove <57889360+cpplovelove@users.noreply.github.com>
2022-12-25 18:19:31 +0900

    [CLIENT][FEAT] 로그 아웃, 위시 생성 페이지 접근 제한
    - 로그 아웃
    - 위시 생성 페이지 접근 제한

--- client/src/pages/main/main.js
+++ client/src/pages/main/main.js
@@ -1,15 +1,19 @@
 import Navigator from "../nav/nav";
 import SunnyImage from "../../assets/sunny.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import JoinModal from "./join-modal";
 import "./main.css";
 import LoginModal from "./login-modal";
-import { UserController } from "../../controller/user-controller";
+import cookies from "react-cookies";
 
 function MainPage() {
   const [joinOpen, setJoinOpen] = useState(false);
   const [loginOpen, setLoginOpen] = useState(false);
+  const [isLogged, setLogged] = useState(false);
 
+  useEffect(() => {
+    if (cookies.load("userInfo")) setLogged(true);
+  });
   function handleJoinOpen() {
     setJoinOpen(!joinOpen);
   }
@@ -30,8 +34,12 @@ function MainPage() {
           <br></br>
           실행까지 100만년 걸리는 당신을 위해 만든 서비스
         </p>
-        <button onClick={handleJoinOpen}>회원 가입</button>
-        <button onClick={handleLoginOpen}>로그인</button>
+        {!isLogged && (
+          <>
+            <button onClick={handleJoinOpen}>회원 가입</button>
+            <button onClick={handleLoginOpen}>로그인</button>
+          </>
+        )}
       </div>
       <div className="image">
         <img src={SunnyImage} alt="sunny" />
--- client/src/pages/nav/nav.js
+++ client/src/pages/nav/nav.js
@@ -1,6 +1,28 @@
 import "./nav.css";
+import cookies from "react-cookies";
+import { useState, useEffect } from "react";
 
 function Navigator() {
+  const [isLogged, setLogged] = useState(false);
+  useEffect(() => {
+    if (cookies.load("userInfo")) setLogged(true);
+  });
+
+  function handleLogout() {
+    cookies.remove("userInfo");
+    window.location.href = "/";
+  }
+
+  function checkLogin() {
+    if (!isLogged) {
+      alert("회원가입 이후에 이용할 수있는 서비스 입니다.");
+      window.location.href = "/";
+      return false;
+    } else {
+      window.location.href = "/wish";
+    }
+  }
+
   return (
     <div class="nav">
       <nav class="navbar">
@@ -13,13 +35,24 @@ function Navigator() {
 
         <ul class="navbar__menu">
           <li>
-            <a href="/wish">소원 빌러 가기</a>
+            <button
+              className="nav_button"
+              onClick={() => {
+                checkLogin();
+              }}
+            >
+              소원 빌러 가기
+            </button>
           </li>
         </ul>
 
         <ul class="navbar__icons">
           <li>
-            <i class="fab fa-twitter"></i>
+            {isLogged && (
+              <button className="nav_button" onClick={() => handleLogout()}>
+                로그아웃
+              </button>
+            )}
           </li>
           <li>
             <i class="fab fa-facebook-f"></i>
