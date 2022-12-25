b7fc78f28a2db411857719e2226664b9e44f0661
cpplovelove <57889360+cpplovelove@users.noreply.github.com>
2022-12-25 16:49:43 +0900

    [CLIENT][FEAT] 소원 생성 페이지 수정
    - 소원 생성 페이지 UI 수정

--- client/src/pages/wish/wish.js
+++ client/src/pages/wish/wish.js
@@ -1,37 +1,97 @@
 import Navigator from "../nav/nav";
 import WishModal from "./wish-modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./wish.css";
 
 function WishPage() {
   const [open, setOpen] = useState(true);
+  const [wish, setWish] = useState();
+  const [isExist, setIsExist] = useState(localStorage.getItem("wish") != null);
 
   function handleOpen() {
     setOpen(!open);
   }
 
+  const onChange = (e) => {
+    setWish(e.target.value);
+  };
+
+  function addWish() {
+    if (isExist) {
+      const tempList = [];
+      tempList.push(localStorage.getItem("wish"));
+      tempList.push(wish);
+      localStorage.setItem("wish", tempList);
+    } else {
+      localStorage.setItem("wish", wish);
+    }
+  }
   return (
     <div className="wrapper">
       <Navigator></Navigator>
-      <div className="content">
+      <div className="header">
         <div className="title">운 수 대 통</div>
-
-        <div class="input__section">
+        <div className="input__section">
           <form>
             <div>
-              <input type="text" class="item" autofocus="true" />
+              <input
+                type="text"
+                className="item"
+                autoFocus={true}
+                onChange={onChange}
+                value={wish}
+              />
             </div>
             <div>
-              <button class="input__button">+ </button>
+              <button
+                className="input__button"
+                onClick={() => {
+                  addWish();
+                }}
+              >
+                +
+              </button>
             </div>
           </form>
         </div>
-        {/*  */}
-        <div className="wishList">데이터가 추가 될 예정입니다</div>
-        <button>+</button>
-        <button>d o n e</button>
       </div>
-      {/* <div className="image"><img src={SunnyImage} alt="sunny" /></div> */}
+      <div className="content">
+        <div className="wishList">
+          {isExist &&
+            localStorage.getItem("wish").split(",").length > 0 &&
+            localStorage
+              .getItem("wish")
+              .split(",")
+              .map((item) => {
+                {
+                  return (
+                    <div className="wish">
+                      {item}
+                      <br />
+                    </div>
+                  );
+                }
+              })}
+        </div>
+
+        <table style={{ textAlign: "center" }}>
+          <tr>
+            <td>
+              <button
+                onClick={() => {
+                  localStorage.clear();
+                  setIsExist(false);
+                }}
+              >
+                c l e a r
+              </button>
+            </td>
+            <td>
+              <button>d o n e</button>
+            </td>
+          </tr>
+        </table>
+      </div>
       <WishModal></WishModal>
     </div>
   );
